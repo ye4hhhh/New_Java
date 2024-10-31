3926b84a506dc2b98b519f92cdc8724542553773
Aleksandr Osipov <149664683+ye4hhhh@users.noreply.github.com>
2024-10-31 17:04:01 +0300

    update7

--- practice 7/src/Calendar.java
+++ practice 7/src/Calendar.java
@@ -12,13 +12,13 @@ public class Calendar {
         System.out.println("Текущий месяц: " + month);
         System.out.println("Текущий день: " + day);
 
-        calendar.setTimeInMillis(111222333444L);
+        calendar.setTimeInMillis(1234567898765L);
         year = calendar.get(java.util.Calendar.YEAR);
         month = calendar.get(java.util.Calendar.MONTH) + 1;
         day = calendar.get(java.util.Calendar.DAY_OF_MONTH);
 
-        System.out.println("Год для 111222333444L: " + year);
-        System.out.println("Месяц для 111222333444L: " + month);
-        System.out.println("День для 111222333444L: " + day);
+        System.out.println("Год для 1234567898765L: " + year);
+        System.out.println("Месяц для 1234567898765L: " + month);
+        System.out.println("День для 1234567898765L: " + day);
     }
 }
